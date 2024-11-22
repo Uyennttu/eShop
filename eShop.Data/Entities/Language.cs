c4d441f1122b7a232c583c0fa6dec8276f43f068
ozUyen <ozuyen.nguyen@gmail.com>
2024-11-22 13:57:58 +1000

    add Entities

---
+++ eShop.Data/Entities/Language.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.Data.Entities
+{
+    public class Language
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }    
+        public bool isDefault { get; set; }  
+        public List<ProductTranslation> ProductTranslations { get; set; }
+        public List<CategoryTranslation> CategoryTranslations { get; set; }
+    }
+}
