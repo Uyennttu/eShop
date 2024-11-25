820d66e69efa4f4100a1547bc8c4b79fb84bd113
ozUyen <ozuyen.nguyen@gmail.com>
2024-11-25 11:26:15 +1100

    Migration database

--- eShop.Data/Configurations/OrderDetailConfiguration.cs
+++ eShop.Data/Configurations/OrderDetailConfiguration.cs
@@ -14,8 +14,9 @@ namespace eShop.Data.Configurations
         public void Configure(EntityTypeBuilder<OrderDetail> builder)
         {
             builder.ToTable("OrderDetails");
-            builder.HasKey(x => new { x.OrderId, x.ProductId});
-            builder.HasOne(x=>x.Order).WithMany(x=>x.OrderDetails).HasForeignKey(x=>x.OrderId); 
-            builder.HasOne(x =>x.Product).WithMany(x=> x.OrderDetails).HasForeignKey(x=> x.ProductId);
+            builder.HasKey(x => new { x.OrderId, x.ProductId });
+            builder.HasOne(x => x.Order).WithMany(x => x.OrderDetails).HasForeignKey(x => x.OrderId);
+            builder.HasOne(x => x.Product).WithMany(x => x.OrderDetails).HasForeignKey(x => x.ProductId);
+        }
     }
 }
